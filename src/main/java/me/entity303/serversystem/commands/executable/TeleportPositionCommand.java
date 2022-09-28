77ff660a672bc8ca1f2faeeae6b3f1733dbed0b2
BuildTools <unconfigured@null.spigotmc.org>
2022-09-28 14:15:20 +0200

    Added support for relative teleportation

--- src/main/java/me/entity303/serversystem/commands/executable/TeleportPositionCommand.java
+++ src/main/java/me/entity303/serversystem/commands/executable/TeleportPositionCommand.java
@@ -20,10 +20,12 @@ public class TeleportPositionCommand extends MessageUtils implements CommandExec
             cs.sendMessage(this.getPrefix() + this.getNoPermission(this.Perm("tppos.self")));
             return true;
         }
+
         if (args.length <= 2) {
             cs.sendMessage(this.getPrefix() + this.getSyntax("TPPos", label, cmd.getName(), cs, null));
             return true;
         }
+
         if (args.length == 3) {
             if (!(cs instanceof Player)) {
                 cs.sendMessage(this.getPrefix() + this.getSyntax("TPPos", label, cmd.getName(), cs, null));
@@ -37,11 +39,19 @@ public class TeleportPositionCommand extends MessageUtils implements CommandExec
 
             Location location = this.getLocationFromString(cs, label, cmd.getName(), ((Player) cs), args[0], args[1], args[2]);
 
-            if (location == ((Player) cs).getLocation()) return true;
+            if (!location.getWorld().getWorldBorder().isInside(location))  {
+                //TODO: Send error
+                return true;
+            }
+
+            Location playerLocation = ((Player) cs).getLocation();
+
+            if (location.distance(playerLocation) <= 0)
+                return true;
 
 
             ((Player) cs).teleport(location);
-            cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Self", label, cmd.getName(), cs, null).replace("<X>", String.valueOf(location.getX())).replace("<Y>", String.valueOf(location.getY())).replace("<Z>", String.valueOf(location.getZ())));
+            cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Self", label, cmd.getName(), cs, null).replace("<X>", String.format("%.2f", location.getX())).replace("<Y>", String.format("%.2f", location.getY())).replace("<Z>", String.format("%.2f", location.getZ())));
             return true;
         }
 
@@ -50,6 +60,7 @@ public class TeleportPositionCommand extends MessageUtils implements CommandExec
                 cs.sendMessage(this.getPrefix() + this.getNoPermission(this.Perm("tppos.others")));
                 return true;
             }
+
             Player target = this.getPlayer(cs, args[0]);
 
             if (target == null) {
@@ -59,10 +70,18 @@ public class TeleportPositionCommand extends MessageUtils implements CommandExec
 
             Location location = this.getLocationFromString(cs, label, cmd.getName(), target, args[1], args[2], args[3]);
 
-            if (location == target.getLocation()) return true;
+            if (!location.getWorld().getWorldBorder().isInside(location))  {
+                //TODO: Send error
+                return true;
+            }
+
+            Location playerLocation = target.getLocation();
+
+            if (location.distance(playerLocation) <= 0)
+                return true;
 
             target.getPlayer().teleport(location);
-            cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Others", label, cmd.getName(), cs, target).replace("<X>", String.valueOf(location.getX())).replace("<Y>", String.valueOf(location.getY())).replace("<Z>", String.valueOf(location.getZ())));
+            cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Others", label, cmd.getName(), cs, target).replace("<X>", String.format("%.2f", location.getX())).replace("<Y>", String.format("%.2f", location.getY())).replace("<Z>", String.format("%.2f", location.getZ())));
             return true;
         }
 
@@ -71,6 +90,7 @@ public class TeleportPositionCommand extends MessageUtils implements CommandExec
                 cs.sendMessage(this.getPrefix() + this.getSyntax("TPPos", label, cmd.getName(), cs, null));
                 return true;
             }
+
             if (!this.isAllowed(cs, "tppos.self")) {
                 cs.sendMessage(this.getPrefix() + this.getNoPermission(this.Perm("tppos.self")));
                 return true;
@@ -78,10 +98,19 @@ public class TeleportPositionCommand extends MessageUtils implements CommandExec
 
             Location location = this.getLocationFromString(cs, label, cmd.getName(), ((Player) cs), args[0], args[1], args[2], args[3], args[4]);
 
-            if (location == ((Player) cs).getLocation()) return true;
+
+            if (!location.getWorld().getWorldBorder().isInside(location))  {
+                //TODO: Send error
+                return true;
+            }
+
+            Location playerLocation = ((Player) cs).getLocation();
+
+            if (location.distance(playerLocation) <= 0)
+                return true;
 
             ((Player) cs).teleport(location);
-            cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Self", label, cmd.getName(), cs, null).replace("<X>", String.valueOf(location.getX())).replace("<Y>", String.valueOf(location.getY())).replace("<Z>", String.valueOf(location.getZ())));
+            cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Self", label, cmd.getName(), cs, null).replace("<X>", String.format("%.2f", location.getX())).replace("<Y>", String.format("%.2f", location.getY())).replace("<Z>", String.format("%.2f", location.getZ())));
             return true;
         }
 
@@ -99,15 +128,57 @@ public class TeleportPositionCommand extends MessageUtils implements CommandExec
 
         Location location = this.getLocationFromString(cs, label, cmd.getName(), target, args[1], args[2], args[3], args[4], args[5]);
 
-        if (location == target.getLocation()) return true;
+        if (!location.getWorld().getWorldBorder().isInside(location))  {
+            //TODO: Send error
+            return true;
+        }
+
+        Location playerLocation = target.getLocation();
+
+        if (location.distance(playerLocation) <= 0)
+            return true;
 
         target.getPlayer().teleport(location);
-        cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Others", label, cmd.getName(), cs, null).replace("<X>", String.valueOf(location.getX())).replace("<Y>", String.valueOf(location.getY())).replace("<Z>", String.valueOf(location.getZ())));
+        cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.Success.Others", label, cmd.getName(), cs, null).replace("<X>", String.format("%.2f", location.getX())).replace("<Y>", String.format("%.2f", location.getY())).replace("<Z>", String.format("%.2f", location.getZ())));
         return true;
     }
 
 
     private Location getLocationFromString(CommandSender cs, String label, String command, Player target, String... locationData) {
+        for (int i = 0; i < locationData.length; i++) {
+            if (!locationData[i].startsWith("~"))
+                continue;
+
+            double add = 0;
+            if (locationData[i].length() > 1) {
+                String addition = locationData[i].split("~")[1];
+                try {
+                    add = Double.parseDouble(addition);
+                } catch (NumberFormatException e) {
+                    cs.sendMessage(this.getPrefix() + this.getMessage("TPPos.NotANumber", label, command, cs, target).replace("<NUMBER>", addition));
+                    return target.getLocation();
+                }
+            }
+
+            switch (i) {
+                case 0:
+                    locationData[i] = String.valueOf((target.getLocation().getX() + add));
+                    break;
+                case 1:
+                    locationData[i] = String.valueOf((target.getLocation().getY() + add));
+                    break;
+                case 2:
+                    locationData[i] = String.valueOf((target.getLocation().getZ() + add));
+                    break;
+                case 3:
+                    locationData[i] = String.valueOf((target.getLocation().getYaw() + add));
+                    break;
+                case 4:
+                    locationData[i] = String.valueOf((target.getLocation().getPitch() + add));
+                    break;
+            }
+        }
+
         if (locationData.length == 3) {
             Location location = target.getLocation();
 
