0d2e0aaf006aede08b1f1dbdc9529fe32ab1f369
Entity303 <36412486+TheBlackEntity@users.noreply.github.com>
2022-12-18 17:53:26 +0100

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+echo Checking system...
+SYSTEM=$(command uname)
+
 echo Checking for curl...
 sleep 3
 if ! command -v curl &> /dev/null
@@ -76,7 +79,45 @@ fi
 fi
 fi
 echo Success!
+echo Checking for unzip...
+sleep 3
+if ! command -v unzip &> /dev/null
+then
+    echo "unzip could not be found"
+    if [ "$EUID" -ne 0 ]
+  then echo "Please run as root for automatic installation"
+  exit 1
 
+  else
+    if command -v apt &> /dev/null
+  then
+  apt install unzip
+  else
+  if command -v pacman &> /dev/null
+  then
+  pacman -S unzip
+  else
+  if command -v yum &> /dev/null
+  then
+  yum install unzip
+  else
+  if command -v dnf &> /dev/null
+  then
+  dnf install unzip
+  if command -v zypper &> /dev/null
+  then
+  zypper install unzip
+  else
+  echo "Could not automatically install unzip, please install manually"
+  exit 2
+fi
+fi
+fi
+fi
+fi
+fi
+fi
+echo Success!
 echo Starting ServerSystem project setup...
 echo Cloning ServerSystem git...
 sleep 3
@@ -94,6 +135,8 @@ mkdir 1.8.8
 cp BuildTools.jar 1.8.8/BuildTools.jar
 mkdir 1.9
 cp BuildTools.jar 1.9/BuildTools.jar
+mkdir 1.9.4
+cp BuildTools.jar 1.9.4/BuildTools.jar
 mkdir 1.10.2
 cp BuildTools.jar 1.10.2/BuildTools.jar
 mkdir 1.11.1
@@ -112,114 +155,218 @@ mkdir 1.16.1
 cp BuildTools.jar 1.16.1/BuildTools.jar
 mkdir 1.16.3
 cp BuildTools.jar 1.16.3/BuildTools.jar
+mkdir 1.16.5
+cp BuildTools.jar 1.16.5/BuildTools.jar
 mkdir 1.17
 cp BuildTools.jar 1.17/BuildTools.jar
+mkdir 1.18.2
+cp BuildTools.jar 1.18.2/BuildTools.jar
+mkdir 1.19
+cp BuildTools.jar 1.19/BuildTools.jar
+
+mkdir java17
+cd java17
+echo Downloading Java-17 ...
+sleep 3
+
+if [[ "$SYSTEM" == *"MINGW64_NT"* ]];
+then
+PATH_JAVA_17="jdk-17_windows-x64_bin"
+curl -O https://download.oracle.com/java/17/latest/$PATH_JAVA_17.zip
+else
+PATH_JAVA_17="jdk-17_linux-x64_bin"
+curl -O https://download.oracle.com/java/17/latest/$PATH_JAVA_17.tar.gz
+fi
+
+echo Success!
+echo Extracting Java-17...
+sleep 3
+
+if [[ "$SYSTEM" == *"MINGW64_NT"* ]];
+then
+unzip -o $PATH_JAVA_17.zip
+PATH_JAVA_17="jdk-17.0.5"
+export PATH="$(pwd)/$PATH_JAVA_17/bin:$PATH"
+export JAVA_HOME="$(pwd)/$PATH_JAVA_17/"
+else
+tar -xvf $PATH_JAVA_17.tar.gz
+PATH_JAVA_17="jdk-17.0.5"
+fi
+
+echo Success!
+cd ..
+
 mkdir java16
 cd java16
-echo Downloading Java16...
+echo Downloading Java-16 ...
 sleep 3
-curl -O https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_linux-x64_bin.tar.gz
+
+if [[ "$SYSTEM" == *"MINGW64_NT"* ]];
+then
+PATH_JAVA_16="openjdk-16.0.2_windows-x64_bin"
+curl -O https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/$PATH_JAVA_16.zip
+else
+PATH_JAVA_16="openjdk-16.0.2_linux-x64_bin"
+curl -O https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/$PATH_JAVA_16.tar.gz
+fi
+
 echo Success!
-echo Extracting Java16...
+echo Extracting Java-16...
 sleep 3
-tar -xvf openjdk-16.0.2_linux-x64_bin.tar.gz
+
+if [[ "$SYSTEM" == *"MINGW64_NT"* ]];
+then
+unzip -o $PATH_JAVA_16.zip
+PATH_JAVA_16="jdk-16.0.2"
+export PATH="$(pwd)/$PATH_JAVA_16/bin:$PATH"
+export JAVA_HOME="$(pwd)/$PATH_JAVA_16/"
+else
+tar -xvf $PATH_JAVA_16.tar.gz
+PATH_JAVA_16="jdk-16.0.2"
+fi
+
 echo Success!
 cd ..
 mkdir java8
 cd java8
-echo Downloading Java8...
+echo Downloading Java-8...
 sleep 3
-curl -O https://cdn.azul.com/zulu/bin/zulu8.56.0.21-ca-jdk8.0.302-linux_x64.tar.gz
+
+if [[ "$SYSTEM" == *"MINGW64_NT"* ]];
+then
+PATH_JAVA_8="zulu8.56.0.21-ca-jdk8.0.302-win_x64"
+curl -O https://cdn.azul.com/zulu/bin/$PATH_JAVA_8.zip
+else
+PATH_JAVA_8="zulu8.56.0.21-ca-jdk8.0.302-linux_x64"
+curl -O https://cdn.azul.com/zulu/bin/$PATH_JAVA_8.tar.gz
+fi
+
 echo Success!
-echo Extracting Java8...
+echo Extracting Java-8...
 sleep 3
-tar -xvf zulu8.56.0.21-ca-jdk8.0.302-linux_x64.tar.gz
+
+if [[ "$SYSTEM" == *"MINGW64_NT"* ]];
+then
+unzip -o $PATH_JAVA_8.zip
+else
+tar -xvf $PATH_JAVA_8.tar.gz
+fi
+
 echo Success!
 cd ..
 cd 1.8.8
 echo Building Spigot 1.8.8...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.8.8
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.8.8
 echo Success!
 cd ..
 cp 1.8.8/spigot-1.8.8.jar ../libs/spigot-1.8.8.jar
 cd 1.9
 echo Building Spigot 1.9...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.9
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.9
 echo Success!
 cd ..
 cp 1.9/spigot-1.9.jar ../libs/spigot-1.9.jar
+cd 1.9.4
+echo Building Spigot 1.9.4...
+sleep 3
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.9.4
+echo Success!
+cd ..
+cp 1.9.4/spigot-1.9.4.jar ../libs/spigot-1.9.4.jar
 cd 1.10.2
 echo Building Spigot 1.10.2...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.10.2
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.10.2
 echo Success!
 cd ..
 cp 1.10.2/spigot-1.10.2.jar ../libs/spigot-1.10.2.jar
 cd 1.11.1
 echo Building Spigot 1.11.1...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.11.1
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.11.1
 echo Success!
 cd ..
 cp 1.11.1/spigot-1.11.2.jar ../libs/spigot-1.11.1.jar
 cd 1.12.2
 echo Building Spigot 1.12.2...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.12.2
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.12.2
 echo Success!
 cd ..
 cp 1.12.2/spigot-1.12.2.jar ../libs/spigot-1.12.2.jar
 cd 1.13
 echo Building Spigot 1.13...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.13
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.13
 echo Success!
 cd ..
 cp 1.13/spigot-1.13.jar ../libs/spigot-1.13.jar
 cd 1.13.2
 echo Building Spigot 1.13.2...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.13.2
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.13.2
 echo Success!
 cd ..
 cp 1.13.2/spigot-1.13.2.jar ../libs/spigot-1.13.2.jar
 cd 1.14.4
 echo Building Spigot 1.14.4...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.14.4
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.14.4
 echo Success!
 cd ..
 cp 1.14.4/spigot-1.14.4.jar ../libs/spigot-1.14.4.jar
 cd 1.15.2
 echo Building Spigot 1.15.2...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.15.2
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.15.2
 echo Success!
 cd ..
 cp 1.15.2/spigot-1.15.2.jar ../libs/spigot-1.15.2.jar
 cd 1.16.1
 echo Building Spigot 1.16.1...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.16.1
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.16.1
 echo Success!
 cd ..
 cp 1.16.1/spigot-1.16.1.jar ../libs/spigot-1.16.1.jar
 cd 1.16.3
 echo Building Spigot 1.16.3...
 sleep 3
-./../java8/zulu8.56.0.21-ca-jdk8.0.302-linux_x64/bin/java -jar BuildTools.jar --rev 1.16.3
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.16.3
 echo Success!
 cd ..
 cp 1.16.3/spigot-1.16.3.jar ../libs/spigot-1.16.3.jar
+cd 1.16.5
+echo Building Spigot 1.16.5...
+sleep 3
+./../java8/$PATH_JAVA_8/bin/java -jar BuildTools.jar --rev 1.16.5
+echo Success!
+cd ..
+cp 1.16.5/spigot-1.16.5.jar ../libs/spigot-1.16.5.jar
 cd 1.17
 echo Building Spigot 1.17...
 sleep 3
-./../java16/jdk-16.0.2/bin/java -jar BuildTools.jar --rev 1.17
+./../java16/$PATH_JAVA_16/bin/java -jar BuildTools.jar --rev 1.17 --compile CRAFTBUKKIT,SPIGOT
 echo Success!
 cd ..
 cp 1.17/spigot-1.17.jar ../libs/spigot-1.17.jar
+cd 1.18.2
+echo Building Spigot 1.18.2...
+sleep 3
+./../java17/$PATH_JAVA_17/bin/java -jar BuildTools.jar --rev 1.18.2 --compile CRAFTBUKKIT,SPIGOT
+echo Success!
+cd ..
+cp 1.18.2/Spigot/Spigot-Server/target/spigot-1.18.2-R0.1-SNAPSHOT.jar ../libs/spigot-1.18.2.jar
+cd 1.19
+echo Building Spigot 1.19...
+sleep 3
+./../java17/$PATH_JAVA_17/bin/java -jar BuildTools.jar --rev 1.19 --compile CRAFTBUKKIT,SPIGOT
+echo Success!
+cd ..
+cp 1.19/Spigot/Spigot-Server/target/spigot-1.19-R0.1-SNAPSHOT.jar ../libs/spigot-1.19.jar
+
 cd ..
 echo Spigot dependencies complete!
 echo Downloading PlotSquared...
@@ -244,8 +391,20 @@ mkdir PlotSquaredv6
 cd PlotSquaredv6
 git clone https://github.com/IntellectualSites/PlotSquared.git
 cd PlotSquared
-./gradlew build
-cp build/libs/PlotSquared-Bukkit* ../../../libs/PlotSquared-Bukkit-6.0.6-SNAPSHOT.jar
+./gradlew shadowJar
+cp Bukkit/build/libs/PlotSquared-Bukkit*-SNAPSHOT.jar ../../../libs/PlotSquared-Bukkit-6.0.6-SNAPSHOT.jar
+echo Success!
+sleep 3
+echo Downloading LuckPerms...
+sleep 3
+cd ..
+cd ..
+mkdir LuckPerms
+cd LuckPerms
+curl -o luckperms.zip -L https://ci.lucko.me/job/LuckPerms/lastSuccessfulBuild/artifact/bukkit/loader/build/libs/*/*zip*/libs.zip
+unzip luckperms.zip
+cp LuckPerms-Bukkit* ../../libs/LuckPerms-Bukkit-5.4.54.jar
+cd ..
 echo Success!
 sleep 3
 echo Finished project setup!
