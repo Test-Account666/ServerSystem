ae7e865ee890d37246948512ec8b9bfad5c70454
BuildTools <unconfigured@null.spigotmc.org>
2022-12-18 17:55:25 +0100

    Replaced direct call with Reflections

--- src/main/java/me/entity303/serversystem/listener/plotsquared/PlotListener3.java
+++ src/main/java/me/entity303/serversystem/listener/plotsquared/PlotListener3.java
@@ -1,17 +1,48 @@
 package me.entity303.serversystem.listener.plotsquared;
 
 import com.google.common.eventbus.Subscribe;
-import com.plotsquared.core.api.PlotAPI;
 import com.plotsquared.core.events.PlayerEnterPlotEvent;
 import com.plotsquared.core.events.PlayerLeavePlotEvent;
 import com.plotsquared.core.plot.flag.implementations.TimeFlag;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class PlotListener3 {
 
     public PlotListener3() {
-        PlotAPI plotAPI = new PlotAPI();
-        plotAPI.registerListener(this);
+        Class clazz = null;
+        try {
+            clazz = Class.forName("com.plotsquared.core.api.PlotAPI");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Object plotAPI = null;
+
+        try {
+            plotAPI = clazz.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Method registerListenerMethod = null;
+        try {
+            registerListenerMethod = clazz.getDeclaredMethod("registerListener", Object.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            registerListenerMethod.invoke(plotAPI, this);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
     @Subscribe
