43237ba65efa4568984b44a8183730218529b881
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-06-25 13:03:16 +0200

    Add an actual implementation for registering commands

--- src/main/java/me/testaccount666/serversystem/ServerSystem.java
+++ src/main/java/me/testaccount666/serversystem/ServerSystem.java
@@ -1,9 +1,14 @@
 package me.testaccount666.serversystem;
 
 import me.testaccount666.serversystem.commands.management.CommandManager;
+import me.testaccount666.serversystem.managers.config.ConfigReader;
+import me.testaccount666.serversystem.managers.config.DefaultConfigReader;
 import me.testaccount666.serversystem.userdata.UserManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
 
 public final class ServerSystem extends JavaPlugin {
     public static ServerSystem Instance;
@@ -14,15 +19,31 @@ public final class ServerSystem extends JavaPlugin {
     public void onEnable() {
         Instance = this;
 
+        ConfigReader commandsConfigReader;
+
+        try {
+            commandsConfigReader = loadCommandsConfig();
+        } catch (FileNotFoundException exception) {
+            throw new RuntimeException("Failed loading 'commands.yml'!", exception);
+        }
+
         userManager = new UserManager();
-        commandManager = new CommandManager();
+        commandManager = new CommandManager(commandsConfigReader);
 
         Bukkit.getScheduler().runTaskLater(this, commandManager::registerCommands, 1);
     }
 
+    private ConfigReader loadCommandsConfig() throws FileNotFoundException {
+        var commandsFile = Path.of("plugins", "ServerSystem", "commands.yml").toFile();
+
+        if (!commandsFile.exists()) saveResource("commands.yml", false);
+
+        return new DefaultConfigReader(commandsFile, this);
+    }
+
     @Override
     public void onDisable() {
-
+        if (commandManager != null) commandManager.unregisterCommands();
     }
 
     public UserManager getUserManager() {
--- src/main/java/me/testaccount666/serversystem/commands/management/CommandManager.java
+++ src/main/java/me/testaccount666/serversystem/commands/management/CommandManager.java
@@ -1,23 +1,26 @@
 package me.testaccount666.serversystem.commands.management;
 
 import me.testaccount666.serversystem.ServerSystem;
-import me.testaccount666.serversystem.commands.DefaultTabCompleter;
-import me.testaccount666.serversystem.commands.executables.gamemode.CommandGameMode;
+import me.testaccount666.serversystem.commands.ServerSystemCommand;
 import me.testaccount666.serversystem.commands.interfaces.ServerSystemCommandExecutor;
 import me.testaccount666.serversystem.commands.interfaces.ServerSystemTabCompleter;
 import me.testaccount666.serversystem.commands.wrappers.CommandExecutorWrapper;
 import me.testaccount666.serversystem.commands.wrappers.TabCompleterWrapper;
+import me.testaccount666.serversystem.managers.config.ConfigReader;
+import me.testaccount666.serversystem.utils.ClassList;
 import me.testaccount666.serversystem.utils.ConstructorAccessor;
 import me.testaccount666.serversystem.utils.FieldAccessor;
 import me.testaccount666.serversystem.utils.MethodAccessor;
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -25,7 +28,13 @@ import java.util.function.Function;
 public class CommandManager {
     private final Function<SimpleCommandMap, Map<String, Command>> commandMapAccessor = FieldAccessor.createGetter(SimpleCommandMap.class, "knownCommands");
     private final BiFunction<String, Plugin, PluginCommand> pluginCommandConstructor = ConstructorAccessor.createConstructor(PluginCommand.class, String.class, Plugin.class);
-    private final Consumer<?> syncCommandsAccessor = MethodAccessor.createVoidAccessor(Bukkit.getServer().getClass(), "syncCommands");
+    private final Consumer<? extends Server> syncCommandsAccessor = MethodAccessor.createVoidAccessor(Bukkit.getServer().getClass(), "syncCommands");
+    private final ConfigReader configReader;
+    private final Set<String> registeredCommands = new HashSet<>();
+
+    public CommandManager(ConfigReader configReader) {
+        this.configReader = configReader;
+    }
 
     private Map<String, Command> getCommandMap() {
         var commandMap = (SimpleCommandMap) Bukkit.getCommandMap();
@@ -36,37 +45,108 @@ public class CommandManager {
         return pluginCommandConstructor.apply(name, ServerSystem.Instance);
     }
 
-    private void registerCommand(ServerSystemCommandExecutor command, ServerSystemTabCompleter completer, String... variants) {
+    private void registerCommand(ServerSystemCommandExecutor command, ServerSystemTabCompleter completer, Map<String, List<String>> variantAliasMap) {
         var commandMap = getCommandMap();
 
-        for (var variant : variants) {
-            commandMap.remove(variant);
+        variantAliasMap.forEach((variant, aliases) -> {
+            aliases.forEach(commandMap::remove);
 
             var bukkitCommand = createCommand(variant);
             bukkitCommand.setExecutor(new CommandExecutorWrapper(command));
             bukkitCommand.setTabCompleter(new TabCompleterWrapper(completer));
 
-            commandMap.put(variant, bukkitCommand);
-            commandMap.put("serversystem:${variant}", bukkitCommand);
-        }
+            bukkitCommand.setAliases(aliases);
 
+            for (var alias : aliases) {
+                commandMap.put(alias, bukkitCommand);
+                commandMap.put("serversystem:${alias}", bukkitCommand);
 
+                registeredCommands.add(alias);
+                registeredCommands.add("serversystem:${alias}");
+            }
+        });
     }
 
     public void registerCommands() {
-        //TODO: Remove testing-implementation and replace with actual one
-        var gameModeCommand = new CommandGameMode();
-        var defaultTabCompleter = new DefaultTabCompleter();
+        var commandExecutors = ClassList.getClassesOfType(ServerSystemCommandExecutor.class);
 
-        registerCommand(gameModeCommand, defaultTabCompleter, "gamemode");
+        for (var clazz : commandExecutors) processCommandExecutor(clazz);
 
         syncCommands();
     }
 
+    public void unregisterCommands() {
+        var commandMap = getCommandMap();
+
+        registeredCommands.forEach(commandMap::remove);
+        registeredCommands.clear();
+    }
+
+    private void processCommandExecutor(Class<?> clazz) {
+        @SuppressWarnings("unchecked")
+        var commandExecutor = (Class<ServerSystemCommandExecutor>) clazz;
+
+        if (!commandExecutor.isAnnotationPresent(ServerSystemCommand.class)) return;
+
+        var commandAnnotation = commandExecutor.getAnnotation(ServerSystemCommand.class);
+        assert commandAnnotation != null;
+        var command = commandAnnotation.name();
+
+        if (!isCommandEnabled(command)) return;
+
+        var variantAliasMap = buildVariantAliasMap(command, commandAnnotation.variants());
+        instantiateAndRegisterCommand(commandExecutor, commandAnnotation.tabCompleter(), variantAliasMap, command);
+    }
+
+    private boolean isCommandEnabled(String command) {
+        return configReader.getBoolean("Commands.${command}.Enabled", false);
+    }
+
+    private boolean isVariantEnabled(String command, String variant) {
+        return configReader.getBoolean("Commands.${command}.Variants.${variant}.Enabled", false);
+    }
+
+    private Map<String, List<String>> buildVariantAliasMap(String command, String[] variants) {
+        var variantAliasMap = new HashMap<String, List<String>>();
+
+        var parentAliases = getAliases("Commands.${command}.Aliases");
+        variantAliasMap.put(command, parentAliases);
+
+        for (var variant : variants) {
+            if (!isVariantEnabled(command, variant)) continue;
+
+            var variantAliases = getAliases("Commands.${command}.${variant}.Aliases");
+            variantAliasMap.put(variant, variantAliases);
+        }
+
+        return variantAliasMap;
+    }
+
+    private List<String> getAliases(String configPath) {
+        return Arrays.stream(configReader.getString(configPath, "").split(","))
+                .map(String::trim)
+                .filter(alias -> !alias.isEmpty())
+                .toList();
+    }
+
+    private void instantiateAndRegisterCommand(Class<? extends ServerSystemCommandExecutor> commandExecutor,
+                                               Class<? extends ServerSystemTabCompleter> tabCompleter,
+                                               Map<String, List<String>> variantAliasMap,
+                                               String command) {
+        try {
+            registerCommand(
+                    commandExecutor.getDeclaredConstructor().newInstance(),
+                    tabCompleter.getDeclaredConstructor().newInstance(),
+                    variantAliasMap
+            );
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException exception) {
+            throw new RuntimeException("Error registering command '${command}'!", exception);
+        }
+    }
+
     private void syncCommands() {
-        var server = Bukkit.getServer();
         //noinspection unchecked,rawtypes
-        ((Consumer) syncCommandsAccessor).accept(server);
+        ((Consumer) syncCommandsAccessor).accept(Bukkit.getServer());
 
         Bukkit.getOnlinePlayers().forEach(Player::updateCommands);
     }
